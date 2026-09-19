4d4287be2d63e3991c358ca1006ccbc5054006e8
Rajais2 <kamsingh05@yahoo.com>
2026-09-18 17:44:46 -0700

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,57 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # A simple way of representing all possible
+        # movements
+        directions = [
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1)
+        ]
+        
+        # Creating our DFS recursive function
+        def DFS(row, col, word_index):
+            # Checking if we are in bounds
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
+                return False
+
+            # Checking if we have visited this cell already
+            if board[row][col] == '#':
+                return False
+
+            # Checking if this cell doesn't match the
+            # next part of our word
+            if board[row][col] != word[word_index]:
+                return False
+
+            # We found the word!
+            if word_index + 1 == len(word):
+                return True
+
+            # Marking this cell as visited
+            original = board[row][col]
+            board[row][col] = '#'
+
+            # Traversing through all possible directions
+            for dr, dc in directions:
+                # Calculating our new row and col positions
+                new_row = row + dr
+                new_col = col + dc
+
+                # We have found a cell for the next character
+                if (DFS(new_row, new_col, word_index + 1)):
+                    board[row][col] = original
+                    return True
+
+            # Tried all directions but none worked
+            board[row][col] = original
+            return False
+
+        # Traversing through the board to find the first character
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    if (DFS(row, col, 0)):
+                        return True
+
+        return False
